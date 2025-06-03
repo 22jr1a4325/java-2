ed0bccf2fb77231bedb415d49b6f43b29db6bbbf
22jr1a4325 <22jr1a4325@gmail.com>
2025-06-03 16:37:47 +0530

    Add files via upload

---
+++ arrays found.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String[] bikes= {"HD", "BMW", "RE", "DUCATI", "APRILLA"};
+        String search="HD";
+        boolean found= false;
+        Arrays.sort(bikes);
+         for(String bike: bikes)
+         {
+             if(bike.equalsIgnoreCase(search))
+       {
+           found = true;
+           break;
+       }
+         }
+       if(found)
+       {
+          System.out.println(search+ "is found");   
+       }
+       else
+       {
+           System.out.println(search+ "is not found"); 
+       }
+    }
+}
+       
