ff88a8960d23bca49b9f5f6a5274b2f8d7af78d7
SowmyaGarnepudi <22jr1a4325@gmail.com>
2025-06-06 10:32:56 +0530

    Add files via upload

---
+++ ao.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Arthmetic{
+    void perform() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Addition: "+ (a+b));
+        System.out.println("Subtract: "+ (a-b));
+        System.out.println("Product: "+ (a*b));
+        System.out.println("Division: "+ (a/b));
+    }
+}
+public class ao{
+    public static void main(String[] args){
+        Arthmetic obj = new Arthmetic();
+        obj.perform();
+    }
+}
